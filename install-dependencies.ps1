5d3b5b9f050689f567066b1c1d12da2ab7dcac08
Omglolyes <raiden.erdmann@gmail.com>
2021-01-22 19:22:47 +0100

    trim stuff

--- install-dependencies.ps1
+++ install-dependencies.ps1
@@ -5,6 +5,9 @@ param (
 if (Test-Path (Join-Path -Path $path -Childpath 'requirements.txt')) {
     Get-Content 'requirements.txt' | ForEach-Object {
         $module = $_ -split "="
+        for($i = 0; $i -le $module.count; $i++){
+            $module[$i] = ($module[$i] | Out-String).Trim()
+        }
         if ($module.count -gt 1) {
             Install-Module -Name $module[0] -MinimumVersion $module[1] -Force -SkipPublisherCheck
         }
