b11d283ffc0fdf47d35bb50a597c497a9dc33bfa
Omglolyes <raiden.erdmann@gmail.com>
2021-01-22 19:18:52 +0100

    no function, just a script

--- install-dependencies.ps1
+++ install-dependencies.ps1
@@ -1,20 +1,18 @@
-function Install-Dependencies {
-    [CmdletBinding()]
-    param (
-        [string]$path = $PWD
-    )
-    if (Test-Path (Join-Path -Path $path -Childpath 'requirements.txt')) {
-        Get-Content 'requirements.txt' | ForEach-Object {
-            $module = $_ -split "="
-            if ($module.count -gt 1) {
-                Install-Module -Name $module[0] -MinimumVersion $module[1] -Force -SkipPublisherCheck
-            }
-            else {
-                Install-Module -Name $module[0] -Force -SkipPublisherCheck
-            }
+[CmdletBinding()]
+param (
+    [string]$path = $PWD
+)
+if (Test-Path (Join-Path -Path $path -Childpath 'requirements.txt')) {
+    Get-Content 'requirements.txt' | ForEach-Object {
+        $module = $_ -split "="
+        if ($module.count -gt 1) {
+            Install-Module -Name $module[0] -MinimumVersion $module[1] -Force -SkipPublisherCheck
+        }
+        else {
+            Install-Module -Name $module[0] -Force -SkipPublisherCheck
         }
     }
-    else {
-        Write-Host "requirements.txt not found"
-    }
+}
+else {
+    Write-Host "requirements.txt not found"
 }
