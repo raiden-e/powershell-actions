adbd2dc8c64bfbf907798f21f4330da686f63f68
Omglolyes <raiden.erdmann@gmail.com>
2021-01-22 19:25:57 +0100

    try catch

--- install-dependencies.ps1
+++ install-dependencies.ps1
@@ -5,8 +5,13 @@ param (
 if (Test-Path (Join-Path -Path $path -Childpath 'requirements.txt')) {
     Get-Content 'requirements.txt' | ForEach-Object {
         $module = $_ -split "="
-        for($i = 0; $i -le $module.count; $i++){
-            $module[$i] = ($module[$i] | Out-String).Trim()
+        try {
+            for ($i = 0; $i -le $module.count; $i++) {
+                $module[$i] = ($module[$i] | Out-String).Trim()
+            }
+        }
+        catch {
+            Write-Host "Only one Module"
         }
         if ($module.count -gt 1) {
             Install-Module -Name $module[0] -MinimumVersion $module[1] -Force -SkipPublisherCheck
