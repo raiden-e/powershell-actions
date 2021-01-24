4eac931babaf48fa83132613dc83bfee7382f79c
Omglolyes <raiden.erdmann@gmail.com>
2021-01-24 23:33:56 +0100

    bump 11.1.6

--- build-docs.ps1
+++ build-docs.ps1
@@ -1,99 +1,100 @@
 [CmdletBinding()]
 param(
-    [parameter(Mandatory = $true, Position = 0)] [string] $path,
+    [parameter(Mandatory = $true, Position = 0)]  [string] $path,
     [ValidateSet("Confluence", "HTML", "Markdown", IgnoreCase = $true)]
-    [parameter(Mandatory = $false, Position = 1)] [string] $template = "Markdown",
-    [parameter(Mandatory = $false, Position = 2)] [string] $outputDir = './help',
-    [parameter(Mandatory = $false, Position = 3)] [string] $subdir
+    [parameter(Mandatory = $false, Position = 1)] [string] $subdir,
+    [parameter(Mandatory = $true, Position = 2)]  [string] $outputDir,
+    [parameter(Mandatory = $false, Position = 3)] [string] $template = "Markdown"
 )
-function Repair-String ($in = '', [bool]$includeBreaks = $false) {
-    if ($in -eq $null) { return }
 
-    $rtn = $in.Replace('&', '&amp;').Replace('<', '&lt;').Replace('>', '&gt;').Trim()
+function Import-Help {
+    param (
+        $moduleName
+    )
+    $commandsHelp = (Get-Command -module $moduleName) | Get-Help -Full | Where-Object { ! $_.name.EndsWith('.ps1') }
 
-    if ($includeBreaks) {
-        $rtn = $rtn.Replace([Environment]::NewLine, '<br>')
-    }
-    return $rtn
-}
-
-$commandsHelp = (Get-Command -module $moduleName) | Get-Help -Full | Where-Object { ! $_.name.EndsWith('.ps1') }
-
-if ([string]::IsNullOrWhiteSpace($commandsHelp)) {
-    $commandsHelp = (Get-Command $moduleName -ErrorAction SilentlyContinue) | Get-Help -Full
     if ([string]::IsNullOrWhiteSpace($commandsHelp)) {
-        try {
-            $commandsHelp = Get-Help .\$moduleName -Full
-            # $commandsHelp.details.name = (Resolve-Path .\$moduleName -ErrorAction SilentlyContinue).basename
-        }
-        catch {
+        $commandsHelp = (Get-Command $moduleName -ErrorAction SilentlyContinue) | Get-Help -Full
+        if ([string]::IsNullOrWhiteSpace($commandsHelp)) {
             try {
-                Get-Help .\$moduleName.ps1 -Full
-                # $commandsHelp.details.name = (Resolve-Path .\$moduleName.ps1 -ErrorAction Stop).basename
+                $commandsHelp = Get-Help .\$moduleName -Full
+                # $commandsHelp.details.name = (Resolve-Path .\$moduleName -ErrorAction SilentlyContinue).basename
             }
             catch {
-                throw "ERROR: Command's help file is empty"
+                try {
+                    Get-Help .\$moduleName.ps1 -Full
+                    # $commandsHelp.details.name = (Resolve-Path .\$moduleName.ps1 -ErrorAction Stop).basename
+                }
+                catch {
+                    throw "ERROR: Command's help file is empty"
+                }
             }
         }
     }
-}
 
-foreach ($help in $commandsHelp) {
-    $cmdHelp = (Get-Command $help.Name)
+    foreach ($help in $commandsHelp) {
+        $cmdHelp = (Get-Command $help.Name)
 
-    # Get any aliases associated with the method
-    $alias = Get-Alias -Definition $help.Name -ErrorAction SilentlyContinue
-    if ($alias) {
-        $help | Add-Member Alias $alias
+        # Get any aliases associated with the method
+        $alias = Get-Alias -Definition $help.Name -ErrorAction SilentlyContinue
+        if ($alias) {
+            $help | Add-Member Alias $alias
+        }
+
+        # Parse the related links and assign them to a links hashtable.
+        if (($help.relatedLinks | Out-String).Trim().Length -gt 0) {
+            $links = $help.relatedLinks.navigationLink | ForEach-Object {
+                if ($_.uri) { @{name = $_.uri; link = $_.uri; target = '_blank' } }
+                if ($_.linkText) { @{name = $_.linkText; link = "#$($_.linkText)"; cssClass = 'psLink'; target = '_top' } }
+            }
+            $help | Add-Member Links $links
+        }
+
+        # Add parameter aliases to the object.
+        foreach ($p in $help.parameters.parameter ) {
+            $paramAliases = ($cmdHelp.parameters.values | Where-Object name -like $p.name | Select-Object aliases).Aliases
+            if ($paramAliases) {
+                $p | Add-Member Aliases "$($paramAliases -Join ', ')" -Force
+            }
+        }
     }
 
-    # Parse the related links and assign them to a links hashtable.
-    if (($help.relatedLinks | Out-String).Trim().Length -gt 0) {
-        $links = $help.relatedLinks.navigationLink | ForEach-Object {
-            if ($_.uri) { @{name = $_.uri; link = $_.uri; target = '_blank' } }
-            if ($_.linkText) { @{name = $_.linkText; link = "#$($_.linkText)"; cssClass = 'psLink'; target = '_top' } }
-        }
-        $help | Add-Member Links $links
-    }
-
-    # Add parameter aliases to the object.
-    foreach ($p in $help.parameters.parameter ) {
-        $paramAliases = ($cmdHelp.parameters.values | Where-Object name -like $p.name | Select-Object aliases).Aliases
-        if ($paramAliases) {
-            $p | Add-Member Aliases "$($paramAliases -Join ', ')" -Force
-        }
+    $totalCommands = $commandsHelp.Count
+    if (!$totalCommands) {
+        $totalCommands = 1
     }
 }
 
 # Create the output directory if it does not exist
+if (!$outputDir) {
+    throw "No output directory provided"
+}
 if (!(Test-Path $outputDir)) {
     New-Item -Path $outputDir -ItemType Directory | Out-Null
 }
 
-$totalCommands = $commandsHelp.Count
-if (!$totalCommands) {
-    $totalCommands = 1
-}
-
+# Load template functions
 try {
-    Import-Module $template -Force
+    switch ($template) {
+        "Markdown" {
+            $extension = "md"
+            Join-Path -Path "$PSScriptRoot/src" -ChildPath "Markdown.ps1"   | Import-Module -Force
+        }
+        "Confluence" {
+            $extension = "md"
+            Join-Path -Path "$PSScriptRoot/src" -ChildPath "Confluence.ps1" | Import-Module -Force
+        }
+        "HTML" {
+            $extension = "html"
+            Join-Path -Path "$PSScriptRoot/src" -ChildPath "HTML.ps1"       | Import-Module -Force
+        }
+    }
 }
 catch {
     throw "Could not find template"
 }
 
-switch ($template) {
-    "Markdown" { 
-        $extension = "md"
-    }
-    "Confluence" { 
-        $extension = "md"
-    }
-    "HTML" { 
-        $extension = "html"
-    }
-}
-
+# Create subdirectory if given
 if ($subdir) {
     $testPath = Join-Path -Path $path -ChildPath $subdir
     if (Test-Path $testPath) {
@@ -103,8 +104,10 @@ if ($subdir) {
         Write-Warning "Path does not exist: $testpath"
     }
 }
+
+
 foreach ($script in Get-ChildItem -Path "$path/*" -Include "*.ps1", "*.psm1" -Recurse) {
-    $MarkdownText = ConvertTo-MarkdownDoc -moduleName $moduleName -commandsHelp $commandsHelp
+    $outFile = Import-Help $script | ConvertTo-MarkdownDoc -moduleName $moduleName -commandsHelp $_
     $DocFile = Join-Path $script.Directory.FullName, $script.basename, ".$extension"
-    $MarkdownText | Out-File $DocFile -Encoding utf8 -Force
+    Out-File $DocFile -InputObject $outFile -Encoding utf8 -Force
 }
